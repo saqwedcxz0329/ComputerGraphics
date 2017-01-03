5b76a97a9f7c8a16940629b7b838a244093d3fcb
LouisHsu <r05922162@ntu.edu.tw>
2017-01-03 13:22:13 +0800

    final first

---
+++ final/CG_final/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Transform target;
+	private int waypointIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		target = WayPoints.points[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = target.position - transform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime, Space.World);
+		if (Vector3.Distance (transform.position, target.position) <= 0.4f) {
+			getNextWayPoint ();
+		}
+	}
+
+	void getNextWayPoint(){
+		if (waypointIndex >= WayPoints.points.Length - 1) {
+			Destroy (gameObject);
+			return;
+		} 
+		waypointIndex++;
+		target = WayPoints.points [waypointIndex];
+	}
+}
